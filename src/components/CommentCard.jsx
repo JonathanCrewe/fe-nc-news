3650abf37ec8a7c1773d9dd03e1f17a397dc9368
jonathancrewe <jonathancrewe@bellis-rogers.co.uk>
2024-06-19 10:43:56 +0100

    Q9 - tried but failed to disable delete button.

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../css/Comment.css'
 import { deleteComment } from '../../api'
 
 function CommentCard({comment, defaultUsername,comments, setComments}) {
+    const [isDeleting, setIsDeleting] = useState(true)
+
   const isOwnedComment = comment.author === defaultUsername 
 
   // Functions. 
   async function handleClick(event) {
+    // ToDo - this fails to set the button to disabled while processing. I can't seem to fix it. 
+    // Why can't I just set the event.target.dasabled? This should be easy!
+    setIsDeleting(true)
+
     // Optimistically remove from list.
     const newCommentList = comments.filter( (element) => element.comment_id !== comment.comment_id)
     setComments(newCommentList)
 
     // Remove from DB. 
-    deleteComment(comment.comment_id)
+    await deleteComment(comment.comment_id)
+    setIsDeleting(false)
   }
+
+  // ToDo - this disables botton but won't ever get re-enabled.
+  //useEffect( () => {setIsDeleting(!isDeleting)}, [comments])
 
   // HTML. 
   return (
@@ -21,7 +31,7 @@ function CommentCard({comment, defaultUsername,comments, setComments}) {
         <li className='comment-list' key={comment.comment_id}>
           <p>{comment.body}</p>
           <p>{`By: ${comment.author} | Votes: ${comment.votes}`}</p>
-          {isOwnedComment ? <button onClick={handleClick}>Delete</button> : null}
+          {isOwnedComment ? <button disabled={isDeleting} onClick={handleClick}>{isDeleting ? 'Deleting...' : 'Delete'}</button> : null}
         </li>
     </div>
   )
